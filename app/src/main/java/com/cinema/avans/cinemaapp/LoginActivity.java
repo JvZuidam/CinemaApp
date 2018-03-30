ab0a15b97116b47aefd08761fd15ff2d0b4863ab
Please don't visit my repositories <jimvanzuidam@hotmail.nl>
2018-03-30 10:42:29 +0200

    Changed Login and Main activity layouts

--- app/src/main/java/com/cinema/avans/cinemaapp/LoginActivity.java
+++ app/src/main/java/com/cinema/avans/cinemaapp/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.cinema.avans.cinemaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView textView;
+
+        textView = (TextView) findViewById(R.id.TextViewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent  =new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
